96d341322c2890ba6969c4b25012b93938756527
Jin Ho Jeon <34098750+JJH123123123@users.noreply.github.com>
2024-10-28 22:56:42 +0900

    test : util - Test

--- src/test/java/lib/utilTest.java
+++ src/test/java/lib/utilTest.java
@@ -1,9 +1,11 @@
 
-package util;
+package lib;
 
 import java.util.List;
 import java.util.ArrayList;
 import model.RacingCar;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +15,40 @@ public class utilTest {
     @Test
     public void findWinner() {
         List<RacingCar> racingCarList = new ArrayList<>();
+        racingCarList.add(new RacingCar("A"));
+        racingCarList.add(new RacingCar("B"));
+        racingCarList.add(new RacingCar("C"));
+        racingCarList.add(new RacingCar("D"));
+        racingCarList.add(new RacingCar("E"));
 
-        Util.findWinner(null);
+        racingCarList.get(0).addOne();
+        racingCarList.get(1).addOne();
+        racingCarList.get(2).addOne();
+
+        List<RacingCar> winners = tools.findWinner(racingCarList);
+        for (RacingCar racingCar : winners) {
+            System.out.println(racingCar);
+        }
+        for (RacingCar racingCar : racingCarList
+             ) {
+            Assertions.assertEquals(racingCarList.get(0).getScore(),1);
+        }
+    }
+
+    @DisplayName("random Number Test")
+    @Test
+    void testRandomNumber(){
+        int randomInt = tools.randomInt(10);
+
+        boolean ret = false;
+        for(int i= 0;i<33333;i++){
+            boolean tmp = (1<= randomInt && randomInt<10);
+            if (!tmp){
+                ret = true;
+                break;
+            }
+        }
+
+        Assertions.assertTrue(!ret);
     }
 }
