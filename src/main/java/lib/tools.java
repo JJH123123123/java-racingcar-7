4ca6dad0b2f4a507a801fb0f662299a29ba1d409
Jin Ho Jeon <34098750+JJH123123123@users.noreply.github.com>
2024-10-28 23:01:07 +0900

    feat : 중간출력을 위한 print()

--- src/main/java/lib/tools.java
+++ src/main/java/lib/tools.java
@@ -1,17 +1,20 @@
 
 package lib;
 
+import camp.nextstep.edu.missionutils.test.Assertions;
 import java.util.ArrayList;
 import java.util.List;
 import model.RacingCar;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class tools {
 
-    public static int randomInt(int max){
-        return (int)(Math.random()*max)+1;
+    public static int randomInt(int max) {
+        return (int) (Math.random() * max) + 1;
     }
 
-    public static List<RacingCar> findWinner(List<RacingCar> cars){
+    public static List<RacingCar> findWinner(List<RacingCar> cars) {
         int m = -1;
         int n = cars.size();
         List<RacingCar> winners = new ArrayList<RacingCar>();
@@ -26,7 +29,13 @@ public class tools {
                 winners.add(car);
             }
         }
-
         return winners;
+    }
+
+    public void print(List<RacingCar> cars){
+        for (RacingCar car : cars
+             ) {
+            System.out.println(car.toString());
+        }
     }
 }
